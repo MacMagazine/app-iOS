84ef458c3e87fdaead89e86e9b7b82a4720b3b44
Fernando Saragoca <fsaragoca@me.com>
2016-04-02 10:50:07 -0700

    Install mogenerator if not installed

--- Scripts/generate_model.sh
+++ Scripts/generate_model.sh
@@ -1,1 +1,8 @@
-mogenerator --model ../MacMagazine/Classes/Models/MacMagazine.xcdatamodeld --machine-dir ../MacMagazine/Classes/Models/Machine --output-dir ../MacMagazine/Classes/Models
+#! /usr/bin/env bash
+
+if type mogenerator > /dev/null;
+then
+  mogenerator --model ../MacMagazine/Classes/Models/MacMagazine.xcdatamodeld --machine-dir ../MacMagazine/Classes/Models/Machine --output-dir ../MacMagazine/Classes/Models
+else
+  echo "Please install mogenerator: http://rentzsch.github.io/mogenerator/"
+fi
