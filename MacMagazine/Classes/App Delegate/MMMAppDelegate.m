f4b4ef54074fc0d039eca63dc4a9449438fb012d
Fernando Saragoca <fsaragoca@me.com>
2016-04-02 12:13:37 -0700

    Updated app delegate code style

--- MacMagazine/Classes/App Delegate/MMMAppDelegate.m
+++ MacMagazine/Classes/App Delegate/MMMAppDelegate.m
@@ -12,13 +12,23 @@
 #import "MMMAppDelegate.h"
 #import "SUNCoreDataStore.h"
 
-@interface MMMAppDelegate ()
-
-@end
-
 #pragma mark MMMAppDelegate
 
 @implementation MMMAppDelegate
+
+#pragma mark - Getter/Setters
+
+- (UIWindow *)window {
+    if (!_window) {
+#ifdef DEBUG
+        _window = [[FBTweakShakeWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+#else
+        _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+#endif
+    }
+
+    return _window;
+}
 
 #pragma mark - Application Lifecycle
 
@@ -26,21 +36,13 @@
     NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"MacMagazine" withExtension:@"momd"];
     [SUNCoreDataStore setupDefaultStoreWithModelURL:modelURL persistentStoreURL:nil];
     [AFNetworkActivityIndicatorManager sharedManager].enabled = YES;
-    
-    [self.window setTintColor:[UIColor colorWithRed:0.25 green:0.66 blue:0.96 alpha:1]];
+
+    self.window.tintColor = [UIColor colorWithRed:0.25 green:0.66 blue:0.96 alpha:1];
     
     return YES;
 }
 
 #pragma mark FBTweaks UI Override
 
-#ifdef DEBUG
-- (UIWindow *)window {
-    if (!_window) {
-        _window = [[FBTweakShakeWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    }
-    
-    return _window;
-}
-#endif
+
 @end
