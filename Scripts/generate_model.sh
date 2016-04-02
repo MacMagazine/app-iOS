0e84ee0ce871a1cb948dd5e17fc50ad11f80835b
Fernando Saragoca <fsaragoca@me.com>
2016-04-02 10:46:23 -0700

    Updated mogenerator script

---
+++ Scripts/generate_model.sh
@@ -0,0 +1,1 @@
+mogenerator --model ../MacMagazine/Classes/Models/MacMagazine.xcdatamodeld --machine-dir ../MacMagazine/Classes/Models/Machine --output-dir ../MacMagazine/Classes/Models
--- generate_model.sh
+++
@@ -1,1 +0,0 @@
-mogenerator --v2 --model MacMagazine/Classes/Models/MacMagazine.xcdatamodeld --machine-dir MacMagazine/Classes/Models/Machine --output-dir MacMagazine/Classes/Models
