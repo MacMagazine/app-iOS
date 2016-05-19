87dc83b26669676c578bacb07cd03c2dd0a694ad
Cesar Barscevicius <cesarbars@me.com>
2016-05-19 19:23:46 -0300

    Removes debug timestamps from error messages

--- MacMagazine/Classes/Models/MMMPost.m
+++ MacMagazine/Classes/Models/MMMPost.m
@@ -115,19 +115,7 @@ static NSString * const kMMRSSFeedPath = @"https://macmagazine.com.br/feed/";
             });
         }];
     } failure:^(NSURLSessionDataTask *task, NSError *error) {
-        NSMutableDictionary *userInfo = [error.userInfo mutableCopy];
-
-        NSDateFormatter *dateFormatter = [NSDateFormatter new];
-        dateFormatter.dateFormat = @"dd/MM/yyyy HH:mm:ss";
-
-        dateFormatter.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:[NSTimeZone localTimeZone].secondsFromGMT];
-        NSString *localCurentDateString = [dateFormatter stringFromDate:[NSDate date]];
-        NSString *localStartDateString = [dateFormatter stringFromDate:requestStartDate];
-
-        userInfo[NSLocalizedDescriptionKey] = [NSString stringWithFormat:@"%@\n Request iniciado às :%@ falhou.\n %@",localCurentDateString, localStartDateString, error.localizedDescription];
-        NSError *errorWithDate = [NSError errorWithDomain:error.domain code:error.code userInfo:userInfo];
-
-        if (failure) failure(errorWithDate);
+        if (failure) failure(error);
     }];
 }
 
