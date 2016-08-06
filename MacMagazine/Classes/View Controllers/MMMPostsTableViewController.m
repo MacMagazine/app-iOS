747ae1851616d067b9b36bb604401f311251a16f
Ruyther Costa <ruy_fusion@hotmail.com>
2016-08-06 20:19:28 -0300

    Making the app universal with splitView, to iPhone and iPad.

--- MacMagazine/Classes/View Controllers/MMMPostsTableViewController.m
+++ MacMagazine/Classes/View Controllers/MMMPostsTableViewController.m
@@ -118,7 +118,8 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     NSIndexPath *selectedIndexPath = self.tableView.indexPathForSelectedRow;
     if (selectedIndexPath) {
-        MMMPostDetailViewController *detailViewController = segue.destinationViewController;
+        UINavigationController *navigationController = segue.destinationViewController;
+        MMMPostDetailViewController *detailViewController = (MMMPostDetailViewController *) navigationController.topViewController;
         detailViewController.post = [self.fetchedResultsController objectAtIndexPath:selectedIndexPath];
     }
 
@@ -206,6 +207,13 @@
     return [MMMTableViewHeaderView height];
 }
 
+#pragma mark - UISplitViewDelegate delegate
+
+- (BOOL)splitViewController:(UISplitViewController *)splitViewController collapseSecondaryViewController:(UIViewController *)secondaryViewController ontoPrimaryViewController:(UIViewController *)primaryViewController {
+    
+    return true;
+}
+
 #pragma mark - NSFetchedResultsController delegate
 
 - (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
@@ -216,6 +224,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.splitViewController.delegate = self;
+    self.splitViewController.preferredDisplayMode = UISplitViewControllerDisplayModeAllVisible;
 
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(applyRefreshControlFix)
