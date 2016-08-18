a7aa4eeb63f6747c279d63a14fc9036dd125a8a0
Marcel Mueller <marcel@madeatsampa.com>
2016-08-18 10:58:13 -0300

    Fixes the issues that @fsaragoca pointed

--- MacMagazine/Classes/View Controllers/MMMPostsTableViewController.m
+++ MacMagazine/Classes/View Controllers/MMMPostsTableViewController.m
@@ -162,10 +162,10 @@
         [cell layoutSubviews];
     }
 
-    UIView *bgView = [[UIView alloc] init];
-    [bgView setBackgroundColor:[UIColor colorWithRed:0.98 green:0.98 blue:0.98 alpha:1.00]];
-    [cell setSelectedBackgroundView:bgView];
-    
+    UIView *selectedBackgroundView = [[UIView alloc] init];
+    selectedBackgroundView.backgroundColor = [UIColor colorWithRed:0.98 green:0.98 blue:0.98 alpha:1.00];
+    cell.selectedBackgroundView = selectedBackgroundView;
+
     return cell;
 }
 
