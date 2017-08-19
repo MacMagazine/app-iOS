903b46d2ef049c2da7f92280e8f990fa34dc8d8f
Cassio Rossi <cassio.rossi@kazziosoftware.com>
2017-08-19 11:17:45 -0300

    Setup

---
+++ MacMagazineTests/PostsTests.swift
@@ -0,0 +1,27 @@
+//
+//  PostsTests.swift
+//  MacMagazine
+//
+//  Created by Cassio Rossi on 19/08/17.
+//  Copyright © 2017 MacMagazine. All rights reserved.
+//
+
+import XCTest
+
+class PostsTests: XCTestCase {
+	
+	override func setUp() {
+		super.setUp()
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+	
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		super.tearDown()
+	}
+	
+	func testPost() {
+		// This is an example of a functional test case.
+		// Use XCTAssert and related functions to verify your tests produce the correct results.
+	}
+}
