81cec58f60f2a976ed4fdebe3cd849e4b3e07cd9
Cassio Rossi <cassio.rossi@kazziosoftware.com>
2017-09-03 09:30:32 -0300

    Adição de Unit Tests

--- MacMagazine/Posts.swift
+++ MacMagazine/Posts.swift
@@ -8,11 +8,78 @@
 
 import Foundation
 
-public struct Post {
+class Post: NSObject {
+	
+	// MARK: - Properties
+	
+	public var id: Int
+	public var post_date: Date
+	public var title: String
+	public var content: String
+	
+	// MARK: - Insert Object
+	
+	init(id: Int, post_date: Date, title: String, content: String) {
+		self.id = id
+		self.post_date = post_date
+		self.title = title
+		self.content = content
+	}
+}
 
-	public let id: Int
-	public let date: Date
-	public let title: String
-	public let content: String
+func ==(lhs: Post, rhs: Post) -> Bool {
+	return lhs.id == rhs.id && lhs.post_date == rhs.post_date && lhs.title == rhs.title && lhs.content == rhs.content
+}
 
+class Posts {
+	
+	// Stores all posts here
+	private var posts = [Post]()
+	
+	func isEmpty() -> Bool {
+		return (self.posts.count == 0)
+	}
+	
+	func getNumberOfPosts() -> NSInteger {
+		return self.posts.count
+	}
+	
+	func insertOrUpdatePost(post: Post) -> Void {
+		// Cannot duplicate Ids
+		if let p = self.findPostById(id: post.id) {
+			if let idx = self.getIndexPost(of: p) {
+				self.posts[idx] = post
+			}
+		} else {
+			self.posts.append(post)
+		}
+	}
+	
+	func findPostById(id: Int) -> Post? {
+		let posts = self.posts.filter() { $0.id == id }
+		return (posts.count > 0 ? posts[0] : nil)
+	}
+
+	func deletePost(post: Post) -> Void {
+		self.posts = self.posts.filter() { $0 !== post }
+	}
+	
+	func sortPost() {
+		let dateDescriptor = NSSortDescriptor(key: "post_date", ascending: true)
+		
+		self.posts = (self.posts as NSArray).sortedArray(using: [dateDescriptor]) as! [Post]
+	}
+	
+	func getPostAtIndex(index: NSInteger) -> Post? {
+		if self.getNumberOfPosts() > index {
+			return self.posts[index]
+		} else {
+			return nil
+		}
+	}
+	
+	func getIndexPost(of: Post) -> Int? {
+		return self.posts.index(of: of)
+	}
+	
 }
--- MacMagazineTests/PostsTests.swift
+++ MacMagazineTests/PostsTests.swift
@@ -8,13 +8,16 @@
 
 import XCTest
 
-class PostsTests: XCTestCase {
+class PostTests: XCTestCase {
 	
-	var posts: Posts = []
+	var item: Post?
+	var post_date: Date?
 
 	override func setUp() {
 		super.setUp()
-		// Put setup code here. This method is called before the invocation of each test method in the class.
+		// Put setup codvarere. This method is called before the invocation of each test method in the class.
+		self.post_date = Date()
+		self.item = Post(id: 0, post_date: self.post_date!, title: "title", content: "content")
 	}
 	
 	override func tearDown() {
@@ -22,16 +25,162 @@ class PostsTests: XCTestCase {
 		super.tearDown()
 	}
 	
-	func testPostIsEmpty() {
+	func testThatItemHasId() {
+		XCTAssertEqual(self.item!.id, 0, "ID should always be present")
 	}
 
-	func testSavePost() {
+	func testThatItemHasDate() {
+		XCTAssertEqual(self.item!.post_date, self.post_date, "Date should always be present")
 	}
 
-	func testGetPostTotal() {
+	func testThatItemHasTitle() {
+		XCTAssertEqual(self.item!.title, "title", "Title should always be present")
+	}
+	
+	func testThatItemHasContent() {
+		XCTAssertEqual(self.item!.content, "content", "Content should always be present")
 	}
 
-	func testGetPost() {
+	func testItemCanBeAssignedId() {
+		self.item!.id = 1
+		XCTAssertEqual(self.item!.id, 1, "ID should always be present")
+	}
+	
+	func testItemCanBeAssignedDate() {
+		self.post_date = Date()
+		self.item!.post_date = self.post_date!
+		XCTAssertEqual(self.item!.post_date, self.post_date!, "Date should always be present")
+	}
+	
+	func testItemCanBeAssignedTitle() {
+		self.item!.title = "New title"
+		XCTAssertEqual(self.item!.title, "New title", "Title should always be present")
 	}
 
+	func testItemCanBeAssignedContent() {
+		self.item!.content = "New content"
+		XCTAssertEqual(self.item!.content, "New content", "Content should always be present")
+	}
+
+}
+
+class PostsTests: XCTestCase {
+	
+	var posts: Posts?
+	
+	override func setUp() {
+		super.setUp()
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+		self.posts = Posts()
+	}
+	
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		super.tearDown()
+	}
+	
+	func testIsEmpty() {
+		XCTAssertTrue(self.posts!.isEmpty())
+	}
+	
+	func testGetNumberofPosts() {
+		// Use XCTAssert and related functions to verify your tests produce the correct results.
+		XCTAssertEqual(self.posts?.getNumberOfPosts(), 0)
+	}
+	
+	func testInsertPost() {
+		let p = Post(id: 0, post_date: Date(), title: "title", content: "content")
+		self.posts?.insertOrUpdatePost(post: p)
+		
+		XCTAssertFalse(self.posts!.isEmpty())
+		XCTAssertEqual(self.posts?.getNumberOfPosts(), 1)
+	}
+	
+	func testDeletePost() {
+		// Test for valid data
+		let p = Post(id: 0, post_date: Date(), title: "title", content: "content")
+		self.posts?.insertOrUpdatePost(post: p)
+		
+		XCTAssertFalse(self.posts!.isEmpty())
+		XCTAssertEqual(self.posts?.getNumberOfPosts(), 1)
+		
+		self.posts?.deletePost(post: p)
+		
+		XCTAssertTrue(self.posts!.isEmpty())
+		XCTAssertEqual(self.posts?.getNumberOfPosts(), 0)
+		
+		// Test for wrong data
+		self.posts?.insertOrUpdatePost(post: p)
+		
+		let p1 = Post(id: 1, post_date: Date(), title: "title1", content: "content1")
+		self.posts?.deletePost(post: p1)
+		
+		XCTAssertFalse(self.posts!.isEmpty())
+		XCTAssertEqual(self.posts?.getNumberOfPosts(), 1)
+	}
+	
+	func testGetPostAtIndex() {
+		// Test for valid data
+		let p = Post(id: 0, post_date: Date(), title: "title", content: "content")
+		self.posts?.insertOrUpdatePost(post: p)
+		
+		XCTAssertFalse(self.posts!.isEmpty())
+		XCTAssertEqual(self.posts?.getNumberOfPosts(), 1)
+
+		if let postToCompare = self.posts?.getPostAtIndex(index: 0) {
+			XCTAssertTrue(postToCompare == p)
+		} else {
+			XCTFail("Object is Nil")
+		}
+		
+		// Test for wrong data
+		XCTAssertNil(self.posts?.getPostAtIndex(index: 1))
+	}
+
+	func testFindPostById() {
+		let p = Post(id: 0, post_date: Date(), title: "title", content: "content")
+		self.posts?.insertOrUpdatePost(post: p)
+
+		XCTAssertFalse(self.posts!.isEmpty())
+		XCTAssertEqual(self.posts?.getNumberOfPosts(), 1)
+
+		if let postToCompare = self.posts?.findPostById(id: 0) {
+			XCTAssertTrue(postToCompare == p)
+		} else {
+			XCTFail("Object is Nil")
+		}
+
+		// Test for wrong data
+		XCTAssertNil(self.posts?.findPostById(id: 1))
+	}
+
+	func testGetIndexPost() {
+		let p = Post(id: 0, post_date: Date(), title: "title", content: "content")
+		self.posts?.insertOrUpdatePost(post: p)
+		
+		XCTAssertFalse(self.posts!.isEmpty())
+		XCTAssertEqual(self.posts?.getNumberOfPosts(), 1)
+
+		XCTAssertEqual(self.posts?.getIndexPost(of: p), 0)
+	}
+
+	func testUniqueId() {
+		let p = Post(id: 0, post_date: Date(), title: "title", content: "content")
+		self.posts?.insertOrUpdatePost(post: p)
+		
+		XCTAssertFalse(self.posts!.isEmpty())
+		XCTAssertEqual(self.posts?.getNumberOfPosts(), 1)
+
+		let p1 = Post(id: 0, post_date: Date(), title: "title1", content: "content1")
+		self.posts?.insertOrUpdatePost(post: p1)
+
+		XCTAssertEqual(self.posts?.getNumberOfPosts(), 1)
+		
+		if let postToCompare = self.posts?.getPostAtIndex(index: 0) {
+			XCTAssertTrue(postToCompare == p1)
+		} else {
+			XCTFail("Object is Nil")
+		}
+
+	}
 }
