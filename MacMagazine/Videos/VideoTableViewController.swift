fb4e0c4c091877402c7ce62e5fcfd44dc9b4b391
Cassio Rossi <cassio.rossi@kazziosoftware.com>
2019-04-09 21:20:20 +0100

    Add Videos tab and change settings icon

---
+++ MacMagazine/Videos/VideoTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  VideoTableViewController.swift
+//  MacMagazine
+//
+//  Created by Cassio Rossi on 09/04/2019.
+//  Copyright © 2019 MacMagazine. All rights reserved.
+//
+
+import WatchKit
+
+class VideoTableViewController: UITableViewController {
+
+}
