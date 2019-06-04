0d1415b3047fed7e64595bc7e5a3ab289145cc60
Cassio Rossi <cassio.rossi@kazziosoftware.com>
2019-06-04 19:18:45 +0100

    Fix Photo permissions to avoid crash

--- MacMagazineNotificationServiceExtension/NotificationService.swift
+++ MacMagazineNotificationServiceExtension/NotificationService.swift
@@ -16,7 +16,7 @@ class NotificationService: UNNotificationServiceExtension {
 	var bestAttemptContent: UNMutableNotificationContent?
 
 	override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-		self.receivedRequest = request;
+		self.receivedRequest = request
 		self.contentHandler = contentHandler
 		bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
 
