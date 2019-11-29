9bbb9a8d6e661cdd8f543e0bc9d2208a30f4aca5
Cassio Rossi <cassio.rossi@worldpay.com>
2019-11-29 13:50:39 +0000

    Improve Native Dark Mode

---
+++ MacMagazine/Theme/Extensions/UIAlertControllerExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIAlertViewControllerExtensions.swift
+//  MacMagazine
+//
+//  Created by Cassio Rossi on 29/11/19.
+//  Copyright © 2019 MacMagazine. All rights reserved.
+//
+
+import Foundation
