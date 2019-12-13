7860c7e2ef91481bd70411ec891d9b8739192226
Cassio Rossi <cassio.rossi@kazziosoftware.com>
2019-12-13 20:52:22 +0000

    New build script using fastlane

---
+++ scripts/testflight.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "This is a pull request. No deployment will be done."
+  exit 0
+fi
+
+if [[ "$TRAVIS_BRANCH" != "release/4.0" ]]; then
+  echo "Testing on a branch other than master. No deployment will be done."
+  exit 0
+fi
+
+fastlane matchappstore
+fastlane beta
