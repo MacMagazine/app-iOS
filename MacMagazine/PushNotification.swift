f03bf2152728876081cde89c11f2ae41a991873c
Cassio Rossi <cassio.rossi@kazziosoftware.com>
2023-11-09 13:36:23 +0000

    Add log

--- MacMagazine/PushNotification.swift
+++ MacMagazine/PushNotification.swift
@@ -170,6 +170,8 @@ extension AppDelegate {
                      fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         Database().update()
 
+		logI("applicationState: \(application.applicationState.rawValue)")
+
 		// Only get notification content if the user tap on it
 		if application.applicationState == .inactive {
 			guard let additionalData = userInfo["custom"] as? [String: AnyObject],
