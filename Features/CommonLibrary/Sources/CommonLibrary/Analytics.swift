16cf5be3604378ecfa9bb6b3ca82b1928123b149
Cassio Rossi <cassio.rossi@kazziosoftware.com>
2025-09-09 15:13:58 +0100

    delete all previous code

--- Features/CommonLibrary/Sources/CommonLibrary/Analytics.swift
+++
@@ -1,8 +0,0 @@
-import FirebaseAnalytics
-import Foundation
-
-public struct Analytics {
-	static public func log(settings: [String: AnyObject]) {
-		FirebaseAnalytics.Analytics.logEvent("Settings", parameters: settings)
-	}
-}
