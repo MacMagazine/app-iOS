074cc5036d3d1cb87b33c80d962e4bcac3ce38c1
Cassio Rossi <cassio.rossi@kazziosoftware.com>
2025-12-08 20:51:32 +0000

    Handle data duplication when possible

--- MacMagazine/Features/MMLiveLibrary/Sources/MMLiveLibrary/MMLiveWebView.swift
+++ MacMagazine/Features/MMLiveLibrary/Sources/MMLiveLibrary/MMLiveWebView.swift
@@ -1,5 +1,5 @@
 import SwiftUI
-import UIComponentsLibrary
+ import UIComponentsLibrary
 
 public struct MMLiveWebView: View {
     public init() {}
@@ -10,5 +10,6 @@ public struct MMLiveWebView: View {
             isPresenting: .constant(false),
             standAlone: true
         )
+        .ignoresSafeArea(.container, edges: .bottom)
     }
 }
