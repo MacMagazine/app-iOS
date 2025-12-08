5691f27d0362fc26ce4ba995ac131b0eeb2e8dc5
Cassio Rossi <cassio.rossi@kazziosoftware.com>
2025-12-08 22:53:09 +0000

    Enable silent push notifications

--- MacMagazine/MacMagazine/MainApp/MacMagazineApp.swift
+++ MacMagazine/MacMagazine/MainApp/MacMagazineApp.swift
@@ -2,9 +2,11 @@ import SettingsLibrary
 import StorageLibrary
 import SwiftData
 import SwiftUI
+import UIKit
 
 @main
 struct MacMagazineApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @State var viewModel = MainViewModel()
 
     var body: some Scene {
@@ -14,7 +16,18 @@ struct MacMagazineApp: App {
                 .environment(viewModel)
                 .environment(viewModel.settingsViewModel)
                 .preferredColorScheme(viewModel.settingsViewModel.colorSchema)
+                .task {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
         }
         .environment(\.theme, viewModel.theme)
     }
+}
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {}
+
+    func application(_ application: UIApplication,
+                     didFailToRegisterForRemoteNotificationsWithError error: Error) {}
 }
