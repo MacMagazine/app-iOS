a06e1c7a2218e8a2610ec03290e9b7751f086f58
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2026-01-28 19:04:50 -0300

    Fix optional initialization with 'nil'

--- MacMagazine/Features/NewsLibrary/Sources/NewsLibrary/ViewModel/NewsViewModel.swift
+++ MacMagazine/Features/NewsLibrary/Sources/NewsLibrary/ViewModel/NewsViewModel.swift
@@ -10,7 +10,7 @@ import UIComponentsLibrary
 class NewsViewModel {
     var options: Options = .home
     var status: APIStatus = .idle
-    var selectedNews: FeedDB? = nil
+    var selectedNews: FeedDB?
 
     enum Options: Equatable {
         case home
