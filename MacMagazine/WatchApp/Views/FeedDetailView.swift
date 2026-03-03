4d5133a7effcf7b202a04ee5ff2d5137f7e44b50
Cassio Rossi <cassio.rossi@kazziosoftware.com>
2026-03-03 20:25:18 +0000

    fix issue with a11y

--- MacMagazine/WatchApp/Views/FeedDetailView.swift
+++ MacMagazine/WatchApp/Views/FeedDetailView.swift
@@ -79,7 +79,7 @@ struct FeedDetailView: View {
                 Image(systemName: post.favorite ? "star.fill" : "star")
             }
             .buttonStyle(.glass)
-            .accessibilityLabel(favorite ? "Mostrar tudo" : "Mostrar Favoritos")
+            .accessibilityLabel(post.favorite ? "Mostrar tudo" : "Mostrar Favoritos")
         }
     }
 }
