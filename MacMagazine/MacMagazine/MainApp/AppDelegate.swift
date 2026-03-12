0a65cd0f62d278f2fd67ea85672c3fe89acc8e2b
Cassio Rossi <cassio.rossi@happening.xyz>
2026-03-12 10:24:46 +0000

    trying to fix black window

--- MacMagazine/MacMagazine/MainApp/AppDelegate.swift
+++ MacMagazine/MacMagazine/MainApp/AppDelegate.swift
@@ -17,6 +17,10 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession,
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        if let shortcutItem = options.shortcutItem {
+            ShortcutManager.shared.pendingShortcut = shortcutItem
+        }
+
         let config = UISceneConfiguration(name: nil,
                                           sessionRole: connectingSceneSession.role)
         config.delegateClass = SceneDelegate.self
--- MacMagazine/MacMagazine/MainApp/SceneDelegate.swift
+++ MacMagazine/MacMagazine/MainApp/SceneDelegate.swift
@@ -1,14 +1,6 @@
 import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    func scene(_ scene: UIScene, willConnectTo
-               session: UISceneSession,
-               options connectionOptions: UIScene.ConnectionOptions) {
-        if let shortcutItem = connectionOptions.shortcutItem {
-            ShortcutManager.shared.pendingShortcut = shortcutItem
-        }
-    }
-
     func windowScene(_ windowScene: UIWindowScene,
                      performActionFor shortcutItem: UIApplicationShortcutItem,
                      completionHandler: @escaping (Bool) -> Void) {
