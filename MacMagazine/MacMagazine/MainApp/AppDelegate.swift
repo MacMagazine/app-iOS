2d694094ad4c99aa81baaa8a8d18d22345ce0591
Cassio Rossi <cassio.rossi@kazziosoftware.com>
2026-05-10 19:31:44 +0100

    Finally fixed issue #277 with a caveat, the App must be deleted first

--- MacMagazine/MacMagazine/MainApp/AppDelegate.swift
+++ MacMagazine/MacMagazine/MainApp/AppDelegate.swift
@@ -69,6 +69,11 @@ extension AppDelegate {
     func application(_ application: UIApplication,
                      didReceiveRemoteNotification userInfo: [AnyHashable: Any],
                      fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if userInfo["ck"] != nil {
+            completionHandler(.newData)
+            return
+        }
+
         guard let aps = userInfo["aps"] as? [String: Any],
               let contentAvailable = aps["content-available"] as? Int,
               contentAvailable == 1 else {
